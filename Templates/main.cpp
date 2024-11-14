c8e9c0101a7509e604dcaf199f12fd9c3b76edb0
Tszfai Choy <78771454+TChoy04@users.noreply.github.com>
2024-11-13 20:50:29 -0500

    Update main.cpp

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
  
 int main(){
+	//setIO("problemname");
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	int tc;
